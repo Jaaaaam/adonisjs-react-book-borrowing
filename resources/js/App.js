90fe97b3cb140b06c2402ce7d682408df2005536
Jam <pjdsilvestre@tmj.ph>
2018-10-31 08:40:54 +0800

    initial commit

---
+++ resources/js/App.js
@@ -0,0 +1,15 @@
+console.log('HELLLOOO')
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+           HEEEEEEEEEEY
+        </div>
+    )
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './BookBorrowingApp';
+
+ReactDOM.render(<App />, document.getElementById('root'));
